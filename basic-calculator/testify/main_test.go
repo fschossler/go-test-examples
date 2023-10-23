acfc0834944a5b984e53aa7c6402ba8cf1fd13e6
Felipe Schossler <felipealvesschossler@gmail.com>
2023-10-23 12:53:50 -0300

    feat: add Calculator with testify package

---
+++ basic-calculator/testify/main_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCalculatorAdd(t *testing.T) {
+	// var cal Calculable
+	cal := Calculable(&Calculator{})
+	result := cal.Addition(1, 3)
+
+	assert.Equal(t, 4, result)
+}
+
+func TestCalculatorSubtraction(t *testing.T) {
+	// var cal Calculable
+	cal := Calculable(&Calculator{})
+	result := cal.Subtraction(10, 3)
+
+	assert.Equal(t, 7, result)
+}
+
+func TestCalculatorDivision(t *testing.T) {
+	// var cal Calculable
+	cal := Calculable(&Calculator{})
+	result := cal.Division(10, 5)
+
+	assert.Equal(t, 2, result)
+}
+
+func TestCalculatorMultiplication(t *testing.T) {
+	// var cal Calculable
+	cal := Calculable(&Calculator{})
+	result := cal.Multiplication(2, 5)
+
+	assert.Equal(t, 10, result)
+}
